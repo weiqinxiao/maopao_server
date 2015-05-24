2a1df003b42858bc1befb992c36e4be617ac0ae9
jiangecho <jiangyingji1@163.com>
2015-05-24 10:53:51 +0800

    show user icon in the comment list

--- app/controllers/Comment.java
+++ app/controllers/Comment.java
@@ -5,6 +5,7 @@ import com.tencent.xinge.XingeApp;
 import model.BaseComment;
 import model.CommentListResult;
 import model.CommentResult;
+import model.UserObject;
 import play.db.DB;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -26,20 +27,37 @@ import java.util.Map;
 public class Comment extends Controller{
 
     public static Result comments(long id, int pageSize){
-        String sql = "SELECT * FROM t_comment WHERE tweet_id = %s ORDER BY ID LIMIT %s";
+
+        String sql = "SELECT t_user.id, t_user.name, t_user.head_url, t_comment.id, t_comment.content, t_comment.create_at, t_user.created_at " +
+                "FROM t_user INNER JOIN t_comment ON t_user.id = t_comment.owner_id WHERE tweet_id = %s ORDER BY t_comment.id ASC LIMIT %s";
         sql = String.format(sql, id, pageSize);
 
         Connection connection = DB.getConnection();
-        ResultSet resultSet = null;
-        Statement statement = null;
+        ResultSet commentResultSet = null;
+        Statement commentStatement = null;
         CommentListResult commentListResult = new CommentListResult();
         try {
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(sql);
+            commentStatement = connection.createStatement();
             List<BaseComment> comments = new ArrayList<BaseComment>();
 
-            while (resultSet.next()){
-                comments.add(new BaseComment(resultSet));
+            BaseComment comment;
+            commentResultSet = commentStatement.executeQuery(sql);
+            while (commentResultSet.next()){
+                //BaseComment(String id, String owner_id, String tweet_id, String content, String created_at)
+                comment = new BaseComment(commentResultSet.getString(4),
+                        commentResultSet.getString(1),
+                        id + "",
+                        commentResultSet.getString(5),
+                        commentResultSet.getTimestamp(6).getTime()
+                );
+                //UserObject(long id, String name, String headImgUrl, long created_at){
+                comment.owner = new UserObject(commentResultSet.getLong(1),
+                        commentResultSet.getString(2),
+                        commentResultSet.getString(3),
+                        commentResultSet.getLong(7)
+                );
+                comments.add(comment);
+
             }
 
             commentListResult.setCode(0);
@@ -49,9 +67,17 @@ public class Comment extends Controller{
             e.printStackTrace();
         }finally {
             try {
-                resultSet.close();
-                statement.close();
-                connection.close();
+                if (commentResultSet != null){
+                    commentResultSet.close();
+                }
+
+                if (commentStatement != null){
+                    commentStatement.close();
+                }
+
+                if (connection != null){
+                    connection.close();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
