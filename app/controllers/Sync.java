9dea5ae4df4e7f1d180567203c0246924beff7b2
jiangecho <jiangyingji1@163.com>
2015-10-27 20:59:04 +0800

    add sync support

--- app/controllers/Sync.java
+++ app/controllers/Sync.java
@@ -6,11 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import constant.Constant;
 import model.SyncRecord;
 import model.SyncRecordList;
+import play.db.DB;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import util.DBUtil;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,11 +48,18 @@ public class Sync extends Controller {
         }
 
         // t_train and t_challenge 用户存储用户每天的数据，不是每次的数据
-        final String sql = "INSERT INTO t_train(owner_id, start, end) VALUES('%s', %s, %s)";
+        String table;
+        if (syncRecordList.getTable().equals("t_train")) {
+            table = "t_train";
+        } else {
+            table = "t_challenge";
+        }
+
+        final String sql = "INSERT INTO " + table + " (owner_id, date, duration) VALUES('%s', %s, %d) ON DUPLICATE KEY UPDATE duration = %d";
         String tmp;
         List<String> sqls = new ArrayList<>();
         for (SyncRecord record : syncRecordList.getRecords()) {
-            tmp = String.format(sql, uid, record.getDate(), record.getDuration());
+            tmp = String.format(sql, uid, record.getDate(), record.getDuration(), record.getDuration());
             sqls.add(tmp);
         }
         int count = DBUtil.bulkInsert(sqls);
@@ -58,4 +70,50 @@ public class Sync extends Controller {
         }
         return ok(Json.toJson(result));
     }
+
+    //http://localhost:9000/api/sync/download/t_train?limit=1
+    public static Result download(String table, int limit) {
+        //String owner_id = session("id");
+        String owner_id = "1";
+        SyncRecordList syncRecordList = new SyncRecordList();
+        if (owner_id == null || owner_id.length() == 0) {
+            syncRecordList.setCode(Constant.UN_LOGIN);
+            return ok(Json.toJson(syncRecordList));
+        }
+
+        Connection connection = DB.getConnection();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String sql = "SELECT date, duration from " + table + " WHERE owner_id = 1 order BY id DESC limit " + limit;
+            ResultSet resultSet = statement.executeQuery(sql);
+            SyncRecord syncRecord;
+            while (resultSet.next()) {
+                syncRecord = new SyncRecord();
+                syncRecord.setDate(resultSet.getString("date"));
+                syncRecord.setDuration(resultSet.getLong("duration"));
+                syncRecordList.addRecord(syncRecord);
+            }
+            syncRecordList.setCode(0);
+            syncRecordList.setTable(table);
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return ok(Json.toJson(syncRecordList));
+    }
+
+
 }
--- app/model/SyncRecord.java
+++ app/model/SyncRecord.java
@@ -4,12 +4,10 @@ package model;
  * Created by jiangecho on 15/10/25.
  */
 public class SyncRecord {
-    private int id;
     private String date;
     private long duration;
 
-    public SyncRecord(int id, String date, long duration) {
-        this.id = id;
+    public SyncRecord(String date, long duration) {
         this.date = date;
         this.duration = duration;
     }
@@ -17,13 +15,6 @@ public class SyncRecord {
     public SyncRecord() {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getDate() {
         return date;
@@ -31,6 +22,10 @@ public class SyncRecord {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
     }
 
     public long getDuration() {
