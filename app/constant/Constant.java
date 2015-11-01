84a333ceeddf60af99172e5d0d4c73d60738bdee
jiangecho <jiangyingji1@163.com>
2015-11-02 00:07:02 +0800

    support unfollow

--- app/constant/Constant.java
+++ app/constant/Constant.java
@@ -8,4 +8,5 @@ public class Constant {
 
     public static final int ALREADY_FOLLOWED = 2000;
     public static final int FOLLOW_ID_ERROR = 2001;
+    public static final int UN_FOLLOW_FAIL = 2002;
 }
--- app/controllers/User.java
+++ app/controllers/User.java
@@ -82,9 +82,22 @@ public class User extends Controller {
         return ok(Json.toJson(userObject));
     }
 
-    public static Result follow() {
-        String uid = session("id");
-        //String uid = "2";
+    public static Result follow(){
+        return follow(1);
+    }
+
+    public static Result unFollow(){
+        return follow(2);
+    }
+
+    /**
+     *
+     * @param flag 1, follow; 2, unFollow
+     * @return
+     */
+    private static Result follow(int flag) {
+        //String uid = session("id");
+        String uid = "2";
         FollowList followList = new FollowList();
         if (uid == null || uid.trim().length() == 0) {
             followList.setCode(Constant.UN_LOGIN);
@@ -105,25 +118,18 @@ public class User extends Controller {
             return ok(Json.toJson(followList));
         }
 
-        String querySql = "SELECT * FROM t_owner_follow WHERE owner_id = '%s' and follow_owner_id = '%s'";
-        String insertSql = "INSERT INTO t_owner_follow(owner_id, follow_owner_id) VALUES('%s', '%s')";
-
-        insertSql = String.format(insertSql, uid, followId);
-
         Connection connection = DB.getConnection();
         Statement statement = null;
-        ResultSet resultSet;
         try {
             statement = connection.createStatement();
-            querySql = String.format(querySql, uid, followId);
-            resultSet = statement.executeQuery(querySql);
-            if (resultSet.next()) {
-                // all followed
-                followList.setCode(Constant.ALREADY_FOLLOWED);
-            } else {
-                resultSet.close();
-                statement.execute(insertSql);
-                followList.setCode(0);
+            if (flag == 1){
+                followList = follow(statement, uid, followId);
+            }else if (flag == 2){
+                if (unFollow(statement, uid, followId)){
+                    followList.setCode(0);
+                }else {
+                    followList.setCode(Constant.UN_FOLLOW_FAIL);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -144,4 +150,34 @@ public class User extends Controller {
 
         return ok(Json.toJson(followList));
     }
+
+    private static FollowList follow(Statement statement, String uid, String followId) throws SQLException {
+        String querySql = "SELECT * FROM t_owner_follow WHERE owner_id = '%s' and follow_owner_id = '%s'";
+        String insertSql = "INSERT INTO t_owner_follow(owner_id, follow_owner_id) VALUES('%s', '%s')";
+        FollowList followList = new FollowList();
+
+        ResultSet resultSet;
+        querySql = String.format(querySql, uid, followId);
+        insertSql = String.format(insertSql, uid, followId);
+        resultSet = statement.executeQuery(querySql);
+        if (resultSet.next()) {
+            // all followed
+            followList.setCode(Constant.ALREADY_FOLLOWED);
+        } else {
+            resultSet.close();
+            statement.execute(insertSql);
+            followList.setCode(0);
+        }
+        return followList;
+
+    }
+
+    public static boolean unFollow(Statement statement, String uid, String followId) throws SQLException {
+        String deleteSql = "DELETE FROM t_owner_follow WHERE owner_id = '%s' AND follow_owner_id = '%s'";
+        deleteSql = String.format(deleteSql, uid, followId);
+        int result = statement.executeUpdate(deleteSql);
+
+        return result > 0 ? true : false;
+    }
+
 }
